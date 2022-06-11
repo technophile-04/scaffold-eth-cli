0feff9d2e4826fbb5f6bb7b3ed4b2a87e2023bc2
qedk <1994constant@gmail.com>
2022-06-11 13:13:36 +0400

    feat: Add failover support

--- bin/index.js
+++ bin/index.js
@@ -10,6 +10,7 @@ program
   .description(local.description)
   .version(local.version)
   .option('-dir, --directory <name>', 'the name of the new directory to clone into')
+  .option('--https', 'clones scaffold-eth over https instead of the default (ssh)')
 
 program.parse()
 
@@ -18,7 +19,7 @@ const options = program.opts()
 const commandArray = ['clone']
 const sshUrl = 'git@github.com:scaffold-eth/scaffold-eth.git'
 const httpsUrl = 'https://github.com/scaffold-eth/scaffold-eth.git'
-commandArray.push(sshUrl)
+commandArray.push(options.https ? httpsUrl : sshUrl)
 
 if (options.directory !== undefined) {
   commandArray.push(options.directory)
@@ -30,8 +31,8 @@ const child = spawn('git', commandArray, { stdio: 'inherit' })
 
 child.on('close', (code) => {
   if (code !== 0) {
-    console.warn('Cloning via ssh failed... trying with https')
-    commandArray[1] = httpsUrl
+    console.warn(`Cloning via ${options.https ? 'https' : 'ssh'} failed... trying with ${options.https ? 'ssh' : 'https'}`)
+    commandArray[1] = options.https ? sshUrl : httpsUrl
     const child = spawn('git', commandArray, { stdio: 'inherit' })
     child.on('close', (code) => {
       process.exit(code)
